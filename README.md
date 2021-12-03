2924100edbba950ac427303138ce0af3805b6f3c
big.wampa <79389347+bigwampa@users.noreply.github.com>
2021-12-03 12:02:27 -0800

    new scripts

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-
 ## Hardhat Install
-npm install --save-dev @nomiclabs/hardhat-waffle ethereum-waffle chai @nomiclabs/hardhat-ethers ethers @openzeppelin/hardhat-upgrades @openzeppelin/contracts-upgradeable@3.4 @openzeppelin/contracts@3.4 synthetix
+npm install
---
+++ scripts/emergency-withdraw.js
@@ -0,0 +1,31 @@
+const { ethers } = require("hardhat");
+require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
+
+async function main() {
+  const SimpleRewarderPerSec_ABI = [{"inputs": [{"internalType": "contract IERC20","name": "_rewardToken","type": "address"},{"internalType": "contract IERC20","name": "_lpToken","type": "address"},{"internalType": "uint256","name": "_tokenPerSec","type": "uint256"},{"internalType": "contract IMasterChefAxial","name": "_MCA","type": "address"},{"internalType": "bool","name": "_isNative","type": "bool"}],"stateMutability": "nonpayable","type": "constructor"},{"anonymous": false,"inputs": [{"indexed": true,"internalType": "address","name": "user","type": "address"},{"indexed": false,"internalType": "uint256","name": "amount","type": "uint256"}],"name": "OnReward","type": "event"},{"anonymous": false,"inputs": [{"indexed": true,"internalType": "address","name": "previousOwner","type": "address"},{"indexed": true,"internalType": "address","name": "newOwner","type": "address"}],"name": "OwnershipTransferred","type": "event"},{"anonymous": false,"inputs": [{"indexed": false,"internalType": "uint256","name": "oldRate","type": "uint256"},{"indexed": false,"internalType": "uint256","name": "newRate","type": "uint256"}],"name": "RewardRateUpdated","type": "event"},{"inputs": [],"name": "MCA","outputs": [{"internalType": "contract IMasterChefAxial","name": "","type": "address"}],"stateMutability": "view","type": "function"},{"inputs": [],"name": "balance","outputs": [{"internalType": "uint256","name": "","type": "uint256"}],"stateMutability": "view","type": "function"},{"inputs": [],"name": "claimOwnership","outputs": [],"stateMutability": "nonpayable","type": "function"},{"inputs": [],"name": "emergencyWithdraw","outputs": [],"stateMutability": "nonpayable","type": "function"},{"inputs": [],"name": "isNative","outputs": [{"internalType": "bool","name": "","type": "bool"}],"stateMutability": "view","type": "function"},{"inputs": [],"name": "lpToken","outputs": [{"internalType": "contract IERC20","name": "","type": "address"}],"stateMutability": "view","type": "function"},{"inputs": [{"internalType": "address","name": "_user","type": "address"},{"internalType": "uint256","name": "_lpAmount","type": "uint256"}],"name": "onAxialReward","outputs": [],"stateMutability": "nonpayable","type": "function"},{"inputs": [],"name": "owner","outputs": [{"internalType": "address","name": "","type": "address"}],"stateMutability": "view","type": "function"},{"inputs": [],"name": "pendingOwner","outputs": [{"internalType": "address","name": "","type": "address"}],"stateMutability": "view","type": "function"},{"inputs": [{"internalType": "address","name": "_user","type": "address"}],"name": "pendingTokens","outputs": [{"internalType": "uint256","name": "pending","type": "uint256"}],"stateMutability": "view","type": "function"},{"inputs": [],"name": "poolInfo","outputs": [{"internalType": "uint256","name": "accTokenPerShare","type": "uint256"},{"internalType": "uint256","name": "lastRewardTimestamp","type": "uint256"}],"stateMutability": "view","type": "function"},{"inputs": [],"name": "rewardToken","outputs": [{"internalType": "contract IERC20","name": "","type": "address"}],"stateMutability": "view","type": "function"},{"inputs": [{"internalType": "uint256","name": "_tokenPerSec","type": "uint256"}],"name": "setRewardRate","outputs": [],"stateMutability": "nonpayable","type": "function"},{"inputs": [],"name": "tokenPerSec","outputs": [{"internalType": "uint256","name": "","type": "uint256"}],"stateMutability": "view","type": "function"},{"inputs": [{"internalType": "address","name": "newOwner","type": "address"},{"internalType": "bool","name": "direct","type": "bool"},{"internalType": "bool","name": "renounce","type": "bool"}],"name": "transferOwnership","outputs": [],"stateMutability": "nonpayable","type": "function"},{"inputs": [],"name": "updatePool","outputs": [{"components": [{"internalType": "uint256","name": "accTokenPerShare","type": "uint256"},{"internalType": "uint256","name": "lastRewardTimestamp","type": "uint256"}],"internalType": "struct SimpleRewarderPerSec.PoolInfo","name": "pool","type": "tuple"}],"stateMutability": "nonpayable","type": "function"},{"inputs": [{"internalType": "address","name": "","type": "address"}],"name": "userInfo","outputs": [{"internalType": "uint256","name": "amount","type": "uint256"},{"internalType": "uint256","name": "rewardDebt","type": "uint256"},{"internalType": "uint256","name": "unpaidRewards","type": "uint256"}],"stateMutability": "view","type": "function"},{"stateMutability": "payable","type": "receive"}];
+
+  const [signer] = await ethers.getSigners();
+  console.log("emergency withdrawing to the following account:", signer.address);
+  const teddyRewarder_addr = "0xd84f5C83b2Dc67D9aC7Af06A93509CCbe4E81aEa";
+
+  const Rewarder = new ethers.Contract(teddyRewarder_addr, SimpleRewarderPerSec_ABI, signer);
+  const withdraw = await Rewarder.emergencyWithdraw();
+  const tx_withdraw = await withdraw.wait(1);
+  if (!tx_withdraw.status) {
+    console.error(`Error running emergency withdraw`);
+    return;
+  }
+  const newRate = await Rewarder.tokenPerSec();
+  console.log(`successfully ran emergency withdraw`);
+
+  return;
+
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
