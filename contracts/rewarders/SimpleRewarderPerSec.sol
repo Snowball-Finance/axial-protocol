0418285a997e8137b26a3287d81610f0c10535a0
Abigail <abigailtcameron@gmail.com>
2021-12-27 12:30:29 -0500

    quad

--- contracts/rewarders/SimpleRewarderPerSec.sol
+++ contracts/rewarders/SimpleRewarderPerSec.sol
@@ -53,25 +53,30 @@ contract SimpleRewarderPerSec is IRewarder, BoringOwnable, ReentrancyGuard {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
-    IERC20 public immutable override rewardToken;
+    IERC20 public override rewardToken;
+    address public immutable rewardToken1;
+    address public immutable rewardToken2;
+    address public immutable rewardToken3;
+    address public immutable rewardToken4;
     IERC20 public immutable lpToken;
     bool public immutable isNative;
     IMasterChefAxial public immutable MCA;
+
 
     /// @dev Info of each MCA user.
     /// `amount` LP token amount the user has provided.
     /// `rewardDebt` The amount of YOUR_TOKEN entitled to the user.
     struct UserInfo {
         uint256 amount;
-        uint256 rewardDebt;
-        uint256 unpaidRewards;
+        uint256[] rewardDebt; 
+        uint256[] unpaidRewards;
     }
 
     /// @dev Info of each MCA poolInfo.
     /// `accTokenPerShare` Amount of YOUR_TOKEN each LP token is worth.
     /// `lastRewardTimestamp` The last timestamp YOUR_TOKEN was rewarded to the poolInfo.
     struct PoolInfo {
-        uint256 accTokenPerShare;
+        uint256[4] accTokenPerShare; 
         uint256 lastRewardTimestamp;
     }
 
@@ -80,11 +85,11 @@ contract SimpleRewarderPerSec is IRewarder, BoringOwnable, ReentrancyGuard {
     /// @dev Info of each user that stakes LP tokens.
     mapping(address => UserInfo) public userInfo;
 
-    uint256 public tokenPerSec;
+    uint256[] public tokenPerSec;
     uint256 private constant ACC_TOKEN_PRECISION = 1e12;
 
-    event OnReward(address indexed user, uint256 amount);
-    event RewardRateUpdated(uint256 oldRate, uint256 newRate);
+    event OnReward(address indexed user, uint256[] amount);
+    event RewardRateUpdated(uint256[] oldRate, uint256[] newRate);
 
     modifier onlyMCA() {
         require(msg.sender == address(MCA), "onlyMCA: only MasterChefAxial can call this function");
@@ -92,22 +97,36 @@ contract SimpleRewarderPerSec is IRewarder, BoringOwnable, ReentrancyGuard {
     }
 
     constructor(
-        IERC20 _rewardToken,
+        address _rewardToken1,
+        address _rewardToken2,
+        address _rewardToken3,
+        address _rewardToken4,
         IERC20 _lpToken,
-        uint256 _tokenPerSec,
+        uint256[4] memory _tokenPerSec,
         IMasterChefAxial _MCA,
         bool _isNative
     ) public {
-        require(Address.isContract(address(_rewardToken)), "constructor: reward token must be a valid contract");
+        require(Address.isContract(address(_rewardToken1)), "constructor: reward token 1 must be a valid contract");
+        require(Address.isContract(address(_rewardToken2)), "constructor: reward token 2 must be a valid contract");
+        require(Address.isContract(address(_rewardToken3)), "constructor: reward token 3 must be a valid contract");
+        require(Address.isContract(address(_rewardToken4)), "constructor: reward token 4 must be a valid contract");
         require(Address.isContract(address(_lpToken)), "constructor: LP token must be a valid contract");
         require(Address.isContract(address(_MCA)), "constructor: MasterChefAxial must be a valid contract");
 
-        rewardToken = _rewardToken;
+        rewardToken1 = _rewardToken1;
+        rewardToken2 = _rewardToken2;
+        rewardToken3 = _rewardToken3;
+        rewardToken4 = _rewardToken4;
         lpToken = _lpToken;
         tokenPerSec = _tokenPerSec;
         MCA = _MCA;
         isNative = _isNative;
-        poolInfo = PoolInfo({lastRewardTimestamp: block.timestamp, accTokenPerShare: 0});
+        poolInfo = PoolInfo({lastRewardTimestamp: block.timestamp, accTokenPerShare:[uint256(0), uint256(0), uint256(0), uint256(0)]});
+    }
+
+    // function that pushes each of the rewardTokens into an array 
+    function rewardTokenArray(address, address, address, address) public view returns (address[4] memory rewardTokens){
+        rewardTokens = [rewardToken1, rewardToken2, rewardToken3, rewardToken4];
     }
 
     /// @dev Update reward variables of the given poolInfo.
@@ -115,111 +134,145 @@ contract SimpleRewarderPerSec is IRewarder, BoringOwnable, ReentrancyGuard {
     function updatePool() public returns (PoolInfo memory pool) {
         pool = poolInfo;
 
+
         if (block.timestamp > pool.lastRewardTimestamp) {
             uint256 lpSupply = lpToken.balanceOf(address(MCA));
 
             if (lpSupply > 0) {
-                uint256 timeElapsed = block.timestamp.sub(pool.lastRewardTimestamp);
-                uint256 tokenReward = timeElapsed.mul(tokenPerSec);
-                pool.accTokenPerShare = pool.accTokenPerShare.add((tokenReward.mul(ACC_TOKEN_PRECISION) / lpSupply));
-            }
 
+                for (uint i = 0; i < 4; i++){
+                    uint256 timeElapsed = block.timestamp.sub(pool.lastRewardTimestamp);
+                    uint256 tokenReward = timeElapsed.mul(tokenPerSec[i]);
+                    pool.accTokenPerShare[i] = pool.accTokenPerShare[i].add((tokenReward.mul(ACC_TOKEN_PRECISION) / lpSupply));
+                }
+            }
             pool.lastRewardTimestamp = block.timestamp;
             poolInfo = pool;
         }
     }
 
     /// @dev Sets the distribution reward rate. This will also update the poolInfo.
-    /// @param _tokenPerSec The number of tokens to distribute per second
-    function setRewardRate(uint256 _tokenPerSec) external onlyOwner {
+    /// @param _tokenPerSec The number of token 1 to distribute per second
+    function setRewardRate(uint256[] memory _tokenPerSec) external onlyOwner {
         updatePool();
 
-        uint256 oldRate = tokenPerSec;
-        tokenPerSec = _tokenPerSec;
+        uint256[] memory oldRate = new uint256[](4);
 
-        emit RewardRateUpdated(oldRate, _tokenPerSec);
+        for (uint i = 0; i < 4; i++){
+            uint256 oldRates = tokenPerSec[i];
+
+            oldRate[i] = oldRates;
+            tokenPerSec[i] = _tokenPerSec[i];
+        }
+        emit RewardRateUpdated(oldRate, _tokenPerSec); 
     }
 
     /// @dev Function called by MasterChefAxial whenever staker claims AXIAL harvest. Allows staker to also receive a 2nd reward token.
     /// @param _user Address of user
     /// @param _lpAmount Number of LP tokens the user has
     function onAxialReward(address _user, uint256 _lpAmount) external override onlyMCA nonReentrant {
+        address[4] memory rewardTokens = rewardTokenArray(rewardToken1, rewardToken2, rewardToken3, rewardToken4); 
         updatePool();
         PoolInfo memory pool = poolInfo;
         UserInfo storage user = userInfo[_user];
-        uint256 pending;
+        uint256[] memory pending = new uint256[](4);
+        uint256[] memory difference = new uint256[](4);
+    
         if (user.amount > 0) {
-            pending = (user.amount.mul(pool.accTokenPerShare) / ACC_TOKEN_PRECISION).sub(user.rewardDebt).add(
-                user.unpaidRewards
-            );
+            for (uint i = 0; i < 4; i++){
+                uint256 pendings = (user.amount.mul(pool.accTokenPerShare[i]) / ACC_TOKEN_PRECISION).sub(user.rewardDebt[i]).add(
+                        user.unpaidRewards[i]
+                );
+                pending[i] = pendings;
 
-            if (isNative) {
-                uint256 balance = address(this).balance;
-                if (pending > balance) {
-                    (bool success, ) = _user.call.value(balance)("");
-                    require(success, "Transfer failed");
-                    user.unpaidRewards = pending - balance;
+                if (isNative) {
+                    uint256 balance = address(this).balance;
+                    if (pending[i] > balance) {
+                        (bool success, ) = _user.call.value(balance)("");
+                        require(success, "Transfer failed");
+                        user.unpaidRewards[i] = pending[i] - balance;
+                    } else {
+                        (bool success, ) = _user.call.value(pending[i])("");
+                        require(success, "Transfer failed");
+                        user.unpaidRewards[i] = 0;
+                    }
                 } else {
-                    (bool success, ) = _user.call.value(pending)("");
-                    require(success, "Transfer failed");
-                    user.unpaidRewards = 0;
+                    if (rewardTokens[i] != address(0)){
+                        uint256 balance = IERC20(rewardTokens[i]).balanceOf(address(this));
+                        if (pending[i] > balance) {
+                            IERC20(rewardTokens[i]).safeTransfer(_user, balance);
+                            user.unpaidRewards[i] = pending[i] - balance;
+                        } else {
+                            IERC20(rewardTokens[i]).safeTransfer(_user, pending[i]);
+                            user.unpaidRewards[i] = 0;
+                        }
+                    }   
                 }
-            } else {
-                uint256 balance = rewardToken.balanceOf(address(this));
-                if (pending > balance) {
-                    rewardToken.safeTransfer(_user, balance);
-                    user.unpaidRewards = pending - balance;
-                } else {
-                    rewardToken.safeTransfer(_user, pending);
-                    user.unpaidRewards = 0;
-                }
-            }
+                user.amount = _lpAmount;
+                user.rewardDebt[i] = user.amount.mul(pool.accTokenPerShare[i]) / ACC_TOKEN_PRECISION;
+                difference[i] = pending[i] - user.unpaidRewards[i];
+            } 
+            emit OnReward(_user, difference);
         }
-
-        user.amount = _lpAmount;
-        user.rewardDebt = user.amount.mul(pool.accTokenPerShare) / ACC_TOKEN_PRECISION;
-        emit OnReward(_user, pending - user.unpaidRewards);
+           
     }
 
     /// @dev View function to see pending tokens
     /// @param _user Address of user.
     /// @return pending reward for a given user.
     function pendingTokens(address _user) external view override returns (uint256 pending) {
+
+        return 0; 
+        
+    }
+
+    function pending(address _user) public returns (uint256[] memory pending){
         PoolInfo memory pool = poolInfo;
         UserInfo storage user = userInfo[_user];
+        uint256[] memory accTokenPerShare = new uint256[](4);
 
-        uint256 accTokenPerShare = pool.accTokenPerShare;
-        uint256 lpSupply = lpToken.balanceOf(address(MCA));
+        for (uint i = 0; i < 4; i++){
+            accTokenPerShare[i] = pool.accTokenPerShare[i]; 
+            uint256 lpSupply = lpToken.balanceOf(address(MCA));
 
-        if (block.timestamp > pool.lastRewardTimestamp && lpSupply != 0) {
-            uint256 timeElapsed = block.timestamp.sub(pool.lastRewardTimestamp);
-            uint256 tokenReward = timeElapsed.mul(tokenPerSec);
-            accTokenPerShare = accTokenPerShare.add(tokenReward.mul(ACC_TOKEN_PRECISION).div(lpSupply));
+            if (block.timestamp > pool.lastRewardTimestamp && lpSupply != 0) {
+                uint256 timeElapsed = block.timestamp.sub(pool.lastRewardTimestamp);
+                uint256 tokenReward = timeElapsed.mul(tokenPerSec[i]); 
+                accTokenPerShare[i] = accTokenPerShare[i].add(tokenReward.mul(ACC_TOKEN_PRECISION).div(lpSupply));
+            }
+
+            pending[i] = (user.amount.mul(accTokenPerShare[i]) / ACC_TOKEN_PRECISION).sub(user.rewardDebt[i]).add(
+                user.unpaidRewards[i]
+            );
         }
-
-        pending = (user.amount.mul(accTokenPerShare) / ACC_TOKEN_PRECISION).sub(user.rewardDebt).add(
-            user.unpaidRewards
-        );
+        return pending; 
     }
+    
 
     /// @dev In case rewarder is stopped before emissions finished, this function allows
     /// withdrawal of remaining tokens.
     function emergencyWithdraw() public onlyOwner {
+        address[4] memory rewardTokens = rewardTokenArray(rewardToken1, rewardToken2, rewardToken3, rewardToken4); 
+
         if (isNative) {
             (bool success, ) = msg.sender.call.value(address(this).balance)("");
             require(success, "Transfer failed");
         } else {
-            rewardToken.safeTransfer(address(msg.sender), rewardToken.balanceOf(address(this)));
+            for (uint i = 0; i < 4; i++){
+                IERC20(rewardTokens[i]).safeTransfer(address(msg.sender), IERC20(rewardTokens[i]).balanceOf(address(this)));   
+            }
         }
     }
 
     /// @dev View function to see balance of reward token.
-    function balance() external view returns (uint256) {
-        if (isNative) {
-            return address(this).balance;
-        } else {
-            return rewardToken.balanceOf(address(this));
-        }
+    function balance() external view returns (uint256[] memory balances) {
+        address[4] memory rewardTokens = rewardTokenArray(rewardToken1, rewardToken2, rewardToken3, rewardToken4); 
+
+            for (uint i = 0; i < 4; i++){
+                uint256 bal = IERC20(rewardTokens[i]).balanceOf(address(this));
+                balances[i] = bal;
+                return balances;
+            }
     }
 
     /// @dev payable function needed to receive AVAX
---
+++ test/fixtures.js
@@ -0,0 +1,359 @@
+const { deployments, ethers } = require("hardhat")
+const helpers = require('./helpers')
+const addresses = require('./addresses.json')
+
+TRACER_ENABLED = process.argv.includes('--logs')
+const { assets, unilikeFactories, curvelikePools, unilikeRouters } = addresses
+let ADAPTERS = {}
+
+const _synapseAdapter = async () => {
+    const [ deployer ] = await ethers.getSigners()
+    // Import live contracts
+    const SynapsePool = await ethers.getContractAt('ISynapse', curvelikePools.SynapseDAIeUSDCeUSDTeNUSD)
+    // Init Adapters
+    const  SynapseAdapterFactory = await ethers.getContractFactory('SynapseAdapter')
+    const SynapseAdapter =  await SynapseAdapterFactory.connect(deployer).deploy(
+        'Synapse YakAdapter',
+        curvelikePools.SynapseDAIeUSDCeUSDTeNUSD, 
+        2e5
+    )
+    return {
+        SynapseAdapterFactory,
+        SynapseAdapter,
+        SynapsePool,
+        deployer
+    }
+    
+}
+const _curvelikeAdapters = async () => {
+    const [ deployer ] = await ethers.getSigners()
+    // Import live contracts
+    const pools = {
+        GondolaUSDTUSDTe: await ethers.getContractAt('ICurveLikePool', curvelikePools.GondolaUSDTUSDTe),
+        GondolaUSDTeUSDCe: await ethers.getContractAt('ICurveLikePool', curvelikePools.GondolaUSDTeUSDCe),
+        GondolaUSDTeTSD: await ethers.getContractAt('ICurveLikePool', curvelikePools.GondolaUSDTeTSD),
+        GondolaUSDT: await ethers.getContractAt('ICurveLikePool', curvelikePools.GondolaUSDT),
+        GondolaDAI: await ethers.getContractAt('ICurveLikePool', curvelikePools.GondolaDAI),
+        GondolaBTC: await ethers.getContractAt('ICurveLikePool', curvelikePools.GondolaBTC),
+        GondolaETH: await ethers.getContractAt('ICurveLikePool', curvelikePools.GondolaETH),
+        SnobF3D: await ethers.getContractAt('ICurveLikePool', curvelikePools.snobF3D),
+        SnobS3D: await ethers.getContractAt('ICurveLikePool', curvelikePools.snobS3D),
+        SnobS4D: await ethers.getContractAt('ICurveLikePool', curvelikePools.snobS4D),
+    }
+    // Init Adapters
+    const adapters = {}
+    const  CurveLikeAdapterFactory = await ethers.getContractFactory('CurveLikeAdapter')
+    adapters['SnobF3DAdapter'] = await CurveLikeAdapterFactory.connect(deployer).deploy(
+        'SnobF3D YakAdapter',
+        curvelikePools.snobF3D, 
+        170000
+    )
+    adapters['SnobS3DAdapter'] = await CurveLikeAdapterFactory.connect(deployer).deploy(
+        'SnobS3D YakAdapter',
+        curvelikePools.snobS3D, 
+        170000
+    )
+    adapters['SnobS4DAdapter'] = await CurveLikeAdapterFactory.connect(deployer).deploy(
+        'SnobS4D YakAdapter',
+        curvelikePools.snobS4D, 
+        180000
+    )
+    adapters['GondolaDAIAdapter'] = await CurveLikeAdapterFactory.connect(deployer).deploy(
+        'GondolaDAI YakAdapter',
+        curvelikePools.GondolaDAI, 
+        150000
+    )
+    adapters['GondolaBTCAdapter'] = await CurveLikeAdapterFactory.connect(deployer).deploy(
+        'GondolaBTC YakAdapter',
+        curvelikePools.GondolaBTC, 
+        150000
+    )
+    adapters['GondolaUSDTAdapter'] = await CurveLikeAdapterFactory.connect(deployer).deploy(
+        'GondolaUSDT YakAdapter',
+        curvelikePools.GondolaUSDT, 
+        150000
+    )
+    adapters['GondolaETHAdapter'] = await CurveLikeAdapterFactory.connect(deployer).deploy(
+        'GondolaETH YakAdapter',
+        curvelikePools.GondolaETH, 
+        150000
+    )
+    adapters['GondolaUSDTeUSDCeAdapter'] = await CurveLikeAdapterFactory.connect(deployer).deploy(
+        'GondolaUSDTeUSDCe YakAdapter',
+        curvelikePools.GondolaUSDTeUSDCe, 
+        150000
+    )
+    adapters['GondolaUSDTeTSDAdapter'] = await CurveLikeAdapterFactory.connect(deployer).deploy(
+        'GondolaUSDTeTSD YakAdapter',
+        curvelikePools.GondolaUSDTeTSD, 
+        150000
+    )
+    adapters['GondolaUSDTUSDTeAdapter'] = await CurveLikeAdapterFactory.connect(deployer).deploy(
+        'GondolaUSDTUSDTe YakAdapter',
+        curvelikePools.GondolaUSDTUSDTe, 
+        150000
+    )
+    ADAPTERS = {...adapters, ...ADAPTERS}
+    // Set tags
+    if (TRACER_ENABLED) {
+        hre.tracer.nameTags['deployer'] = deployer
+        for (key of Object.keys(adapters)) {
+            hre.tracer.nameTags[adapters[key].address] = key
+        }
+    }
+    return {
+        CurveLikeAdapterFactory,
+        adapters, 
+        pools,
+    }
+}
+const _unilikeAdapters = async () => {
+    const [ deployer ] = await ethers.getSigners()
+    const adapters = {}
+    const UnilikeAdapterFactory = await ethers.getContractFactory('UnilikeAdapter')
+    adapters['ElkAdapter'] = await UnilikeAdapterFactory.connect(deployer).deploy(
+        'Elk YakAdapter',
+        unilikeFactories.elk,
+        3,
+        100000
+    )
+    adapters['PandaAdapter'] = await UnilikeAdapterFactory.connect(deployer).deploy(
+        'PandaSwap YakAdapter',
+        unilikeFactories.pandaswap,
+        3,
+        100000
+    )
+    adapters['OliveAdapter'] = await UnilikeAdapterFactory.connect(deployer).deploy(
+        'OliveSwap YakAdapter',
+        unilikeFactories.olive,
+        2,
+        100000
+    )
+    adapters['SushiswapAdapter'] = await UnilikeAdapterFactory.connect(deployer).deploy(
+        'Sushiswap YakAdapter',
+        unilikeFactories.sushiswap,
+        3,
+        100000
+    )
+    adapters['ComplusAdapter'] = await UnilikeAdapterFactory.connect(deployer).deploy(
+        'Complus YakAdapter',
+        unilikeFactories.complus,
+        3,
+        100000
+    )
+    adapters['ZeroAdapter'] = await UnilikeAdapterFactory.connect(deployer).deploy(
+        'Zero YakAdapter',
+        unilikeFactories.zero,
+        3,
+        100000
+    )
+    adapters['PangolinAdapter'] = await UnilikeAdapterFactory.connect(deployer).deploy(
+        'Pangolin YakAdapter',
+        unilikeFactories.pangolin,
+        3,
+        100000
+    )
+    adapters['LydiaAdapter'] = await UnilikeAdapterFactory.connect(deployer).deploy(
+        'Lydia YakAdapter',
+        unilikeFactories.lydia,
+        2,
+        100000
+    )
+    adapters['YetiAdapter'] = await UnilikeAdapterFactory.connect(deployer).deploy(
+        'Yeti YakAdapter',
+        unilikeFactories.yeti,
+        3,
+        100000
+    )
+    adapters['JoeAdapter'] = await UnilikeAdapterFactory.connect(deployer).deploy(
+        'Joe YakAdapter',
+        unilikeFactories.joe,
+        3,
+        100000
+    )
+    ADAPTERS = {...adapters, ...ADAPTERS}
+    // Set tags
+    if (TRACER_ENABLED) {
+        hre.tracer.nameTags['deployer'] = deployer
+        for (key of Object.keys(adapters)) {
+            hre.tracer.nameTags[adapters[key].address] = key
+        }
+    }
+    return {
+        UnilikeAdapterFactory, 
+        adapters,
+    }
+}
+const _bridgeMigrationAdapters = async () => {
+    const [ deployer ] = await ethers.getSigners()
+    const adapterFactory = await ethers.getContractFactory('BridgeMigrationAdapter')
+    const oldTokens = [
+        await ethers.getContractFactory('TestToken').then(f => f.deploy('TToken1', 'TT1')),
+        await ethers.getContractFactory('TestToken').then(f => f.deploy('TToken2', 'TT2'))
+    ]
+    const bridgeTokens = [
+        await ethers.getContractFactory('BridgeToken').then(f => f.deploy('BToken1', 'BT1')),
+        await ethers.getContractFactory('BridgeToken').then(f => f.deploy('BToken1', 'BT1'))
+    ]
+    for (let i=0; i<bridgeTokens.length; i++) {
+        const bt = bridgeTokens[i]
+        const ot = oldTokens[i]
+        await bt.addSwapToken(ot.address, ethers.utils.parseUnits('300'))
+    }
+    const gasCost = 71000
+    adapter = await adapterFactory.connect(deployer).deploy(
+        bridgeTokens.map(t => t.address),
+        oldTokens.map(t => t.address),
+        gasCost
+    )
+    // Set tags
+    if (TRACER_ENABLED) {
+        hre.tracer.nameTags['deployer'] = deployer
+        hre.tracer.nameTags[adapter.address] = 'BridgeMigrationAdapter'
+    }
+    return {
+        adapterFactory, 
+        adapter,
+        oldTokens,
+        bridgeTokens
+    }
+}
+const _miniYakAdapter = async () => {
+    const gasCost = 81000
+    const [ deployer ] = await ethers.getSigners()
+    const adapterFactory = await ethers.getContractFactory('MiniYakAdapter')
+    const adapter = await adapterFactory.connect(deployer).deploy(gasCost)
+    const tkns = {}
+    tkns['YAK'] = await helpers.getTokenContract(assets.YAK)
+    tkns['mYAK'] = await helpers.getTokenContract(assets.mYAK)
+    // Set tags
+    if (TRACER_ENABLED) {
+        hre.tracer.nameTags['deployer'] = deployer
+        hre.tracer.nameTags[adapter.address] = 'MiniYakAdapter'
+        for (key in tkns) {
+            hre.tracer.nameTags[tkns[key].address] = key
+        }
+    }
+    return {
+        adapterFactory, 
+        adapter,
+        tkns
+    }
+}
+const general = deployments.createFixture(async () => {
+    // Get token contracts
+    const tokenContracts = {}
+    for (tknSymbol of Object.keys(assets)) {
+        tokenContracts[tknSymbol] = await helpers.getTokenContract(assets[tknSymbol])
+    }
+    // Get accounts
+    const genNewAccount = await helpers.makeAccountGen()
+    const deployer = genNewAccount()
+    // Two dexes to help with testing token->token trades
+    const PangolinRouter = await ethers.getContractAt('IUnilikeAVAXRouter', unilikeRouters.pangolin)
+    const LydiaRouter = await ethers.getContractAt('IUnilikeAVAXRouter', unilikeRouters.lydia)
+    const SushiswapRouter = await ethers.getContractAt('IUnilikeETHRouter', unilikeRouters.sushiswap)
+    const ZeroRouter = await ethers.getContractAt('IUnilikeETHRouter', unilikeRouters.zero)
+    // Set tags
+    if (TRACER_ENABLED) {
+        hre.tracer.nameTags[deployer.address] = 'deployer'
+        for (key of Object.keys(assets)) {
+            hre.tracer.nameTags[assets[key]] = key
+        }
+        for (key of Object.keys(curvelikePools)) {
+            hre.tracer.nameTags[curvelikePools[key]] = key
+        }
+        for (key of Object.keys(unilikeRouters)) {
+            hre.tracer.nameTags[unilikeRouters[key]] = key
+        }
+    }
+    // Constants
+    const U256_MAX  = ethers.constants.MaxUint256
+    const ZERO  = ethers.constants.Zero
+
+    return {
+        unilikeFactories,
+        SushiswapRouter,
+        tokenContracts,
+        unilikeRouters,
+        curvelikePools,
+        PangolinRouter,
+        genNewAccount,
+        LydiaRouter,
+        ZeroRouter,
+        deployer,
+        U256_MAX,
+        assets,
+        ZERO
+    }
+})
+const miniYakAdapter = deployments.createFixture(_miniYakAdapter)
+const synapseAdapter = deployments.createFixture(_synapseAdapter)
+const unilikeAdapters = deployments.createFixture(_unilikeAdapters)
+const curvelikeAdapters = deployments.createFixture(_curvelikeAdapters)
+const bridgeMigration = deployments.createFixture(_bridgeMigrationAdapters)
+const router = deployments.createFixture(async ({ }) => {
+    const [ deployer ] = await ethers.getSigners()
+    const _unilike = await _unilikeAdapters()
+    const _curvelike = await _curvelikeAdapters()
+    const _bridgeMigration = await _bridgeMigrationAdapters()
+
+    let adapters
+    if (ADAPTERS.length>0) {
+        adapters = ADAPTERS
+    } else {
+        adapters = {
+            ..._unilike.adapters,
+            ..._curvelike.adapters,
+            'BridgeMigration': _bridgeMigration.adapter
+        }
+    }
+    const trustedTokens = [
+        assets.WAVAX,
+        assets.WETHe,
+        assets.USDTe,
+        assets.USDCe,
+        assets.DAIe,
+        assets.WBTCe,
+        assets.LINKe,
+        assets.PNG, 
+        assets.JOE,
+        assets.PEFI,
+        assets.SNOB
+    ]
+    // Deploy the libraries
+    const BytesManipulationFactory = await ethers.getContractFactory('BytesManipulation')
+    const BytesManipulation = await BytesManipulationFactory.deploy()
+    // Init Yak Router
+    const YakRouterFactory = await ethers.getContractFactory('YakRouter', { 
+        libraries: {
+            'BytesManipulation': BytesManipulation.address
+        } 
+    })
+    const YakRouter = await YakRouterFactory.connect(deployer).deploy(
+        Object.values(adapters).map(a=>a.address), 
+        trustedTokens, 
+        deployer.address
+    )
+    // Set tags
+    if (TRACER_ENABLED) {
+        hre.tracer.nameTags['deployer'] = deployer
+        hre.tracer.nameTags[YakRouter.address] = 'YakRouter'
+    }
+
+    return {
+        YakRouterFactory, 
+        YakRouter, 
+        adapters
+    }
+})
+
+module.exports = {
+    curvelikeAdapters, 
+    unilikeAdapters,
+    bridgeMigration,
+    synapseAdapter,
+    miniYakAdapter,
+    general, 
+    router
+}
