80850bcb7098fc5fd345f9d9d324ab360efc639c
alfred <fuzzylemma@gmail.com>
2022-02-02 21:14:34 -0500

    packages from original, prep for tests recongizing avalanche chainid

--- utils/network.ts
+++ utils/network.ts
@@ -2,12 +2,17 @@ export const CHAIN_ID = {
   MAINNET: "1",
   ROPSTEN: "3",
   KOVAN: "42",
+  AVALANCHE: "43114",
   HARDHAT: "31337",
 }
 
 export function isMainnet(networkId: string): boolean {
   return networkId == CHAIN_ID.MAINNET
 }
+
+//export function isAvalanche(networkId: string): boolean {
+//  return networkId == CHAIN_ID.AVALANCHE
+//}
 
 export function isTestNetwork(networkId: string): boolean {
   return (
