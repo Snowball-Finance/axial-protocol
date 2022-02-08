b2279ed8b6d5a55718ee418c75510947192ec003
alfred <fuzzylemma@gmail.com>
2022-02-08 14:56:24 -0500

    remove unused import, let to const

--- deploy/utils/deployGaugeV4.ts
+++ deploy/utils/deployGaugeV4.ts
@@ -2,7 +2,7 @@ import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { DeployFunction } from 'hardhat-deploy/types';
 
 export async function deployGaugeV4(hre: HardhatRuntimeEnvironment, deployment_name: string, tokens: string[], rates: string[]) {
-    let contract_name = "GaugeV4";
+    const contract_name = "GaugeV4";
     const {deployments, getNamedAccounts} = hre;
     const {deploy} = deployments;
     const {deployer} = await getNamedAccounts();
